6f388eaccdc6e409586d45b780dec916c79f152a
Josué Andrés Hernández Martínez <hm19065@ues.edu.sv>
2021-06-23 18:00:46 -0600

    Modificar los modelos de Departamento, Municipio y Persona
    - El cambio afecta a todas las tablas, siendo necesario la eliminación
      y recreación de las tablas en la BD.
    
    - Se anexan los nuevos backups de las tablas.
    
    - Se hacen cambios en los templates para hacer uso de las nuevas
      modificaciones.

--- vacunacion/models.py
+++ vacunacion/models.py
@@ -5,26 +5,19 @@ from django.db import models
 
 
 class Departamento(models.Model):
-    codigo_departamento = models.CharField(max_length=2, null=False, default='--', unique=True)
-    nombre = models.CharField(max_length=100, null=False, default='--')
-
-    def __str__(self):
-        return self.nombre
+    codigo_departamento = models.CharField(max_length=2, null=False, default='--', blank=False)
+    nombre = models.CharField(max_length=100, null=False, default='--', blank=False)
 
 
 class Municipio(models.Model):
-    nombre = models.CharField(max_length=100, null=False, default='--')
-    codigo_departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE, to_field='codigo_departamento')
-
-    def __str__(self):
-        return self.nombre
+    nombre = models.CharField(max_length=100, null=False, blank=False, default='--')
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
 
 
 class Persona(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    departamento_de_nacimiento = models.ForeignKey(Departamento, models.DO_NOTHING)
-    municipio_de_nacimiento = models.ForeignKey(Municipio, models.DO_NOTHING)
+    nombres = models.CharField(max_length=100, null=False, blank=False)
     apellidos = models.CharField(max_length=100, null=False, blank=False)
     dui = models.CharField(max_length=10, null=False, blank=False, unique=True)
     fecha_de_nacimiento = models.DateField(blank=False, null=False)
-    nombres = models.CharField(max_length=100, null=False, blank=False)
+    departamento_de_nacimiento = models.ForeignKey(Departamento, on_delete=models.SET_NULL, null=True, to_field='id')
+    municipio_de_nacimiento = models.ForeignKey(Municipio, on_delete=models.SET_NULL, null=True, to_field='id')
