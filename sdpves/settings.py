9fe2fef3fbe63f703562c0a561211f9e82f2950b
Josué Andrés Hernández Martínez <hm19065@ues.edu.sv>
2021-06-23 18:09:41 -0600

    Eliminar libreria 'crispy-forms' y sus configuraciones

--- sdpves/settings.py
+++ sdpves/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'vacunacion.apps.VacunacionConfig',
-    'crispy_forms',
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
